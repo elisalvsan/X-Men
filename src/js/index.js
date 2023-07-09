36174be67e8a4ca2273181fbb26b09a84dcd28dd
Elisa Andrade <elisalvsan@gmail.com>
2023-07-09 15:17:59 -0300

    Refatorando: index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,23 +2,41 @@ const personagens = document.querySelectorAll(".personagem");
 
 personagens.forEach((personagem) => {
   personagem.addEventListener("mouseenter", () => {
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+    if (window.innerWidth < 450) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 
+    removerSelecaoDoPersonagem();
     personagem.classList.add("selecionado");
 
-    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
-
     //alterar imagem
-    const idPersonagem = personagem.attributes.id.value;
-    imagemPersonagemGrande.src = `../../src/assets/img/card-${idPersonagem}.png`;
+    alterarImagemPersonagemSelecionado(personagem);
 
     //alterar nome
-    const nomePersonagem = document.getElementById("nome-personagem");
-    nomePersonagem.innerText = personagem.getAttribute("data-name");
+    alterarNomePersonagemSelecionado(personagem);
 
     //alterar descricao
-    const descricaoPersonagem = document.getElementById("descricao-personagem");
-    descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+    alterarDescricaoPersonagemSelecionado(personagem);
   });
 });
+
+function removerSelecaoDoPersonagem() {
+  const personagemSelecionado = document.querySelector(".selecionado");
+  personagemSelecionado.classList.remove("selecionado");
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+  const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+
+  const idPersonagem = personagem.attributes.id.value;
+  imagemPersonagemGrande.src = `../../src/assets/img/card-${idPersonagem}.png`;
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+  const nomePersonagem = document.getElementById("nome-personagem");
+  nomePersonagem.innerText = personagem.getAttribute("data-name");
+}
+function alterarDescricaoPersonagemSelecionado(personagem) {
+  const descricaoPersonagem = document.getElementById("descricao-personagem");
+  descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+}
